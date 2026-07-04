c170554586f720df76bf222655f9caf0c732c6b3
A-dot-hub <amanjaiswar295@gmail.com>
2026-07-04 13:27:49 +0530

    feat: initialize slider project with React setup and basic components
    - Added package.json with dependencies for React and testing libraries.
    - Created public assets including favicon, index.html, and manifest.json.
    - Implemented basic App structure with App.js and App.css.
    - Added initial test for App component.
    - Created image-slider component structure.
    - Developed styles for image-slider and App components.
    - Implemented star rating component with interactive features and styles.

--- star-rating/src/components/starRating/index.jsx
+++ star-rating/src/components/starRating/index.jsx
@@ -1,20 +1,22 @@
 import { useState } from "react";
 import { FaStar } from "react-icons/fa";
-
+import "./style.css";
 export default function StarRating({ noOfStars = 5 }) {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
   function handleClick(getCurrentIndex) {
     console.log(getCurrentIndex);
+    setRating(getCurrentIndex);
   }
 
   function handleMouseEnter(getCurrentIndex) {
     console.log(getCurrentIndex);
+    setHover(getCurrentIndex);
   }
 
-  function handleMouseLeave(getCurrentIndex) {
-    console.log(getCurrentIndex);
+  function handleMouseLeave() {
+    setHover(rating);
   }
   return (
     <div className="star-rating">
@@ -23,9 +25,10 @@ export default function StarRating({ noOfStars = 5 }) {
         return (
           <FaStar
             key={index}
-            onclick={() => handleClick(index)}
+            className={index <= (hover || rating) ? "active" : "inactive"}
+            onClick={() => handleClick(index)}
             onMouseMove={() => handleMouseEnter(index)}
-            onMouseLeave={() => handleMouseLeave(index)}
+            onMouseLeave={() => handleMouseLeave()}
             size={40}
           />
         );
