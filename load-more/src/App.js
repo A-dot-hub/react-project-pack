c7b617878fdc7c4f06d9f7c5447c85bbbb8d1553
A-dot-hub <amanjaiswar295@gmail.com>
2026-07-06 11:21:19 +0530

    feat: replace StarRating with LoadMoreData component and enhance product fetching logic

--- load-more/src/App.js
+++ load-more/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import StarRating from "./components/starRating";
+import LoadMoreData from "./components/loadmoreData";
 
 function App() {
   return (
     <div className="App">
-      <StarRating noOfStars={10} />
+      <LoadMoreData />
     </div>
   );
 }
