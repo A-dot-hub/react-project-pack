20b4391d60c294d6f175473ae28e191a4cc2192e
A-dot-hub <amanjaiswar295@gmail.com>
2026-07-06 11:44:24 +0530

    feat: implement QR Code generator component and remove StarRating

--- Qr-code-genrator/src/App.js
+++ Qr-code-genrator/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import StarRating from "./components/starRating";
+import QrCodeGenerator from "./components/qr-code";
 
 function App() {
   return (
     <div className="App">
-      <StarRating noOfStars={10} />
+      <QrCodeGenerator />
     </div>
   );
 }
